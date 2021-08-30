317a44a1f4ad929cb1e3db11b641ed048589f569
Sam Price <sjprice@users.noreply.github.com>
2021-08-30 23:46:21 +0800

    Add a few more parser tests

--- src/test/java/au/com/muel/envconfig/DefaultParserRegistryTest.java
+++ src/test/java/au/com/muel/envconfig/DefaultParserRegistryTest.java
@@ -9,6 +9,12 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZonedDateTime;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -53,6 +59,25 @@ class DefaultParserRegistryTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource
+    void testIntegerTypesTooSmall(String s, Class<?> clazz) {
+        assertThrows(NumberFormatException.class, () -> registry.parserForType(clazz).get().parse(s, null));
+    }
+
+    static Stream<Arguments> testIntegerTypesTooSmall() {
+        return Stream.of(
+            arguments("-129", byte.class),
+            arguments("-129", Byte.class),
+            arguments("-32769", short.class),
+            arguments("-32769", Short.class),
+            arguments("-2147483649", int.class),
+            arguments("-2147483649", Integer.class),
+            arguments("-9223372036854775809", long.class),
+            arguments("-9223372036854775809", Long.class)
+        );
+    }
+
     @Test
     void testDecimalTypes() {
         assertEquals(1.2F, registry.parserForType(float.class).get().parse("1.2", null));
@@ -78,7 +103,15 @@ class DefaultParserRegistryTest {
 
     @Test
     void testTimeTypes() {
+        assertEquals(LocalTime.parse("23:10:28.059"), registry.parserForType(LocalTime.class).get().parse("23:10:28.059", null));
+        assertEquals(LocalDate.parse("2021-08-30"), registry.parserForType(LocalDate.class).get().parse("2021-08-30", null));
+        assertEquals(LocalDateTime.parse("2021-08-30T23:37:18.790"), registry.parserForType(LocalDateTime.class).get().parse("2021-08-30T23:37:18.790", null));
         assertEquals(Instant.EPOCH, registry.parserForType(Instant.class).get().parse("1970-01-01T00:00:00Z", null));
+        assertEquals(ZonedDateTime.parse("2021-08-30T23:38:40.436576+08:00[Australia/Perth]"),
+                registry.parserForType(ZonedDateTime.class).get().parse("2021-08-30T23:38:40.436576+08:00[Australia/Perth]", null));
+        assertEquals(OffsetTime.parse("23:39:11.899767+08:00"), registry.parserForType(OffsetTime.class).get().parse("23:39:11.899767+08:00", null));
+        assertEquals(OffsetDateTime.parse("2021-08-30T23:38:57.316746+08:00"),
+                registry.parserForType(OffsetDateTime.class).get().parse("2021-08-30T23:38:57.316746+08:00", null));
     }
 
 }
